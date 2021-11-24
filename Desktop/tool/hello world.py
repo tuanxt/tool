d409848295a14b0775cb6bfb75d31cea323fc907
“tuanxt” <tuanxt95>
2021-11-24 23:17:00 +0700

    test

---
+++ Desktop/tool/hello world.py
@@ -0,0 +1,10 @@
+import os
+
+# Getting the current work directory (cwd)
+thisdir = os.getcwd()
+
+# r=root, d=directories, f = files
+for r, d, f in os.walk(thisdir):
+    for file in f:
+        if ".docx" in file:
+            print(os.path.join(r, file))
